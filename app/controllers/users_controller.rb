f7bb5716e0c90ae1a865b985155c3b7a4bd41d09
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-15 13:23:04 -0500

    "Testing heroku deployment"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 # This controller will also inherit from the ApiController
 class UsersController < ApiController
 	before_action :require_login, except: [:create]
-	
+
 	# create without ! will return boolean
 	# create with ! will return user or error
+
+	def index
+	end
+	
 	def create
 		user = User.create!(user_params)
 		render json:{token: user.auth_token}
 	end
-	
+
 	def profile
 		user = User.find_by_auth_token!(request.headers[:token])
 		render json: {user: { username: user.username, email: user.email, fname: user.fname, lname: user.lname}}
 	end
-	
+
 	private
 	#used to limit accessible columns from user object
 	def user_params
 		params.require(:user).permit(:username, :password, :email, :fname, :lname)
 	end
-	
+
 end
