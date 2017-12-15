a27606869c5eb21523a94117a0b702c3aa64289a
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-15 12:47:54 -0500

    Confirming repo is fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,18 @@
 
 class User < ApplicationRecord
 	validates_uniqueness_of :username
-	attr_reader :fname
-	
+	attr_reader :fname, :lname
+
 	# first method given by bcrypt to handle password hashing
 	# refer to monsters app for detailed look at sessions auth
-	# has_secure_token being passed the column that should have token 
+	# has_secure_token being passed the column that should have token
 	has_secure_password
 	has_secure_token :auth_token
-	
+
 	# this methid is called on the class itself to find the user in the table with given params
 	# not for instances of user class!
 	# authenticate method given by Bcrypt
-	
+
 	#makes use of bcrypt autnenticate method to match hashed password__digest with password attempt
 	def self.validate_login(username, password)
 		user = find_by(username: username)
@@ -22,10 +22,10 @@ class User < ApplicationRecord
 			user
 		end
 	end
-	
+
 	#self is defined in the method to be used on the user instance that calls this method
 	def invalidate_token
 		self.update_columns(auth_token: nil)
 	end
-	
+
 end
