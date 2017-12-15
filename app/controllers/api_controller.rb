7c83a0703a271fcb077e2292fe2249e01ebc6b86
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-14 22:15:17 -0500

    Finished api_controller....currently working on sessions controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,31 @@
+# This controller will be used for reference by other controllers inheriting it's methods
 class ApiController < ApplicationController
+	
 	def require_login
 		authenticate_token || render_unauthorized("Access Denied")
+	end
+	
+	def current_user
+		@current_user ||= authenticate_token
+	end
+	
+	protected
+	
+	def render_unauthorized(message)
+		errors = { errors: [detail: message] }
+		render json: errors, status:  :unauthorized 
+	end
+	
+	private 
+	
+	# this method will find the user by the given token to make sure the current user is correct
+	# authenticate_with_http_token comes from rails,
+	# attaches genereated token to request for user 
+	
+	def authenticate_token 
+		authenticate_with_http_token do | token, options| 
+			User.find_by(auth_token: token)
+		end
+	end
+	
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
-class SessionsController < ApplicationController
+class SessionsController < ApiController
+	skip_before_action :require_login, only: [:create], raise: false
+	
+	def create
+		if user = User.validate_login(params[:username], params[:password])
+			allow_token_to_be_used_only_once_for(user)
+			send_token_for_valid_login_of(user)
+		else
+			render_unauthorized("Error with your login or password")
+		end
+	end
 end
