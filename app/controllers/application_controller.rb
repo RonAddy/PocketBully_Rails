6bf8b09b59190aaddadfd3a5704ee672a55c5fc1
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-15 13:32:53 -0500

    Troubleshooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
 	# This will add tokens for us  (Since Rails 5 does not do this automatically)
 	include ActionController::HttpAuthentication::Token::ControllerMethods
+
+	def index
+	end
+	
 end
