7f5d88182e967928c28e76bcf2eec96ec703ccd2
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-14 21:21:04 -0500

    Created user model and ran first migration (breaking new ground) 🚧 🕳  🚧

---
+++ db/migrate/20171215021502_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.string :fname
+      t.string :lname
+      t.string :auth_token
+
+      t.timestamps
+    end
+  end
+end
