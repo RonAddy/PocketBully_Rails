8349403d42dbd6b17ce65f2b6b48bddfff1f49f6
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-14 22:59:25 -0500

    Added necessary routes for users... working on controller methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
-# methods in the model should handle the 'buisness' dealings with database
+# Model should handle the 'buisness' dealings with database
 # any actions done to database should be done in model
 
 class User < ApplicationRecord
 	validates_uniqueness _of :username
 	
-	# method given by bcrypt to handle password hashing
-	# refer to monsters app for detiled look at sessions auth
+	# first method given by bcrypt to handle password hashing
+	# refer to monsters app for detailed look at sessions auth
 	# has_secure_token being passed the column that should have token 
 	has_secure_password
 	has_secure_token :auth_token
 	
 	# this methid is called on the class itself to find the user in the table with given params
 	# not for instances of user class!
+	# authenticate method given by Bcrypt
+	
+	#makes use of bcrypt autnenticate mthod to match hashed password__digest with password attempt
 	def self.validate_login(username, password)
 		user = find_by(username: username)
 		if user && user.authenticate(password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/login' => "sessions#create"
+  delete '/logout' => "sessions#destroy"
+  
+  resources :users
+  
+  get '/profile' => "users#profile"
+  
 end
