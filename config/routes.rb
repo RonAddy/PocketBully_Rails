7a434eaea09ab92e16e367f733d73fe0483f5435
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-15 13:08:45 -0500

    Testing deployment on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root "application#index"
   post '/login' => "sessions#create"
   delete '/logout' => "sessions#destroy"
-  
+
   resources :users
-  
+
   get '/profile' => "users#profile"
-  
+
 end
